5932c82b567decec091f526569c0739404ad509a
hejarPG <hejar.adak@gmail.com>
2025-01-13 17:46:05 +0330

    Migrate to generic cbv

--- snippets/views.py
+++ snippets/views.py
@@ -7,26 +7,11 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 # Create your views here.
-class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-    
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
     
 
-class SnippetDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet
     serializer_class = SnippetSerializer
-        
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-    
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request,  *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
