4664c854c0c085700ac85cfa51af51c85a867251
hejarPG <hejar.adak@gmail.com>
2025-01-13 06:33:39 +0330

    Fix some minor bugs

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from snippets import views
-
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('snippets/', view=views.snippet_list),
     path('snippets/<int:pk>/', view=views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -9,21 +9,21 @@ from snippets.serializers import SnippetSerializer
 
 # Create your views here.
 @api_view(['GET', 'POST'])
-def snippet_list(request):
+def snippet_list(request, format=None):
     if request.method == 'GET':
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
         return JsonResponse(serializer.data, safe=False)
     if request.method == 'POST':
-        data = JSONParser().parse(request.data)
-        serializer = SnippetSerializer(data=data)
+        print(request.data)
+        serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk):
+def snippet_detail(request, pk, format=None):
     try:
         snippet = Snippet.objects.get(pk=pk)
     except Snippet.DoesNotExist:
